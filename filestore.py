2f3371225c7535a1c79d1bfdfbf03caf3f31e309
Rishi <rishikaley@gmail.com>
2020-05-04 17:03:43 +0530

    Added contaction discovery server

--- filestore.py
+++ filestore.py
@@ -1,6 +1,7 @@
 import click
 import time
-# 1010 for 1mb chunk size
+import socket, sys, requests, json
+
 chunk_size = 50
 
 @click.group()
@@ -9,7 +10,7 @@ def op():
     pass
 
 @op.command()
-@click.option('--filename', type=click.File('r'), help='Path to file to be uploaded', required=True)
+@click.option('--filename', type=click.File('rb'), help='Path to file to be uploaded', required=True)
 # @click.option('--keysfilepath', default='.', type=click.Path(exists=True, resolve_path=True), help='Path to store the recieved keys. Default is current directory')
 @click.option('--keysfilename', type=click.File('w'), help='File name to store the recieved keys. If the file is not in current directory, include path to the file', required=True)#TODO:change to not required and check for overwiting 
 def upload(filename, keysfilename):
@@ -27,11 +28,28 @@ def upload(filename, keysfilename):
             content = f.read(chunk_size)
     click.echo('Closing file...')
 
+    nodeAddr = None
+    nodePort = None
+    #Get node address
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        hostname = '127.0.0.1'
+        s.connect((hostname, 8000))
+        data = b'\x02'
+        print('Formed connection')
+        s.sendall(data)
+        print('Sent data')
+        data = (s.recv(1024)).decode().split('/')
+        print('recved addr ', data[2], ' port ', data[4])
+        nodeAddr = data[2]
+        nodePort = int(data[4])
+
     # Upload to server
     with click.progressbar(chunks, label = 'Uploading to server') as prog_chunks, keysfilename as k:
         for chunk in prog_chunks:
-            # Call api
-            time.sleep(.1)
+            # # # # Call api
+            data = requests.post(url = f'http://{nodeAddr}:{nodePort}', data = ({'json_rpc_method':'dht_getValue', 'key':'abc', 'value':bytes('abc', 'utf-8')}))
+            print(data)
+            # data = data.decode().split('/')
             # Write to keysFileName
             key = 'abc'
             k.write(f'{key}\n')
@@ -40,7 +58,7 @@ def upload(filename, keysfilename):
 
 
 @op.command()
-@click.option('--filename', type=click.File('w'), help='Name to be used for the retrieved file. Default is current directory, include required path in the name to save it at another location', required=True)
+@click.option('--filename', type=click.File('wb'), help='Name to be used for the retrieved file. Default is current directory, include required path in the name to save it at another location', required=True)
 # @click.option('--keysfilepath', default='.', type=click.Path(exists=True, resolve_path=True), help='Path to store the recieved file. Default is current directory')
 @click.option('--keysfilename', type=click.File('r'), help='File name of the keys file. If the file is not in current directory, include path to the file', required=True)
 def retrieve(filename, keysfilename):
