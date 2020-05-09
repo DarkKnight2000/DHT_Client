5864c64b16d8b921c4c2e646593404c29be78c1a
Rishi <rishikaley@gmail.com>
2020-05-09 14:27:20 +0530

    Added data encryption

--- filestore.py
+++ filestore.py
@@ -11,16 +11,16 @@ def op():
     pass
 
 @op.command()
-@click.option('--filename', type=click.File('r'), help='Path to file to be uploaded', required=True)
+@click.option('--filename', type=click.File('rb'), help='Path to file to be uploaded', required=True)
 # @click.option('--keysfilepath', default='.', type=click.Path(exists=True, resolve_path=True), help='Path to store the recieved keys. Default is current directory')
-@click.option('--keysfilename', type=click.File('w'), help='File name to store the recieved keys. If the file is not in current directory, include path to the file', required=True)#TODO:change to not required and check for overwiting 
+@click.option('--keysfilename', type=click.File('wb'), help='File name to store the recieved keys. If the file is not in current directory, include path to the file', required=True)#TODO:change to not required and check for overwiting 
 def upload(filename, keysfilename):
     ''' This command is used to upload files to cloud'''
 
     # Contacting discovery server
     nodeAddr = None
     nodePort = None
-    #Get node address
+    # Get node address
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         hostname = '127.0.0.1'
         s.connect((hostname, 8000))
@@ -46,6 +46,11 @@ def upload(filename, keysfilename):
             content = f.read(chunk_size)
     click.echo('Closing file...')
 
+    # Generating key for encryption
+    fernet_key = Fernet.generate_key()
+    keysfilename.write(fernet_key)
+    keysfilename.write(b"\n")
+
     # nodeAddr = '127.0.0.1'
     # nodePort = 9000
 
@@ -54,28 +59,34 @@ def upload(filename, keysfilename):
         for i, chunk in enumerate(prog_chunks):
             # # # # Call api
             key = f"{i}_key"
-            data = requests.post(url = f'http://{nodeAddr}:{nodePort+1}', json = {'JSONRPCMethod':'dht_putValue', 'Key':key, 'Value':chunk})
+            data = requests.post(url = f'http://{nodeAddr}:{nodePort+1}', json = {'JSONRPCMethod':'dht_putValue', 'Key':key, 'Value':Fernet(fernet_key).encrypt(chunk).decode()}) # Encrypting with key and sending string to server
             # print(data)
             # print(data.text)
             # print(data.status_code)
             # Write to keysFileName
-            k.write(f'{key}\n') #TODO: Check for status code
+            k.write(f'{key}\n'.encode()) #TODO: Check for status code
 
     click.echo('Finished uploading to server! Use this keystore file to retrieve your file contents anytime!')
 
 
 @op.command()
-@click.option('--filename', type=click.File('w'), help='Name to be used for the retrieved file. Default is current directory, include required path in the name to save it at another location', required=True)
+@click.option('--filename', type=click.File('wb'), help='Name to be used for the retrieved file. Default is current directory, include required path in the name to save it at another location', required=True)
 # @click.option('--keysfilepath', default='.', type=click.Path(exists=True, resolve_path=True), help='Path to store the recieved file. Default is current directory')
-@click.option('--keysfilename', type=click.File('r'), help='File name of the keys file. If the file is not in current directory, include path to the file', required=True)
+@click.option('--keysfilename', type=click.File('rb'), help='File name of the keys file. If the file is not in current directory, include path to the file', required=True)
 def retrieve(filename, keysfilename):
     ''' This command is used to retrieve files from cloud using keystore file'''
 
     # Read keystore
     click.echo('Reading keys...')
     keys = []
+    fernet_key = None
     with keysfilename as kf:
-        for key in kf: keys.append(key.strip())
+        fernet_key = kf.readline().strip()
+        print('fkey', fernet_key)
+        while True:
+            key = kf.readline()
+            if not key : break
+            keys.append(key.strip().decode())
     click.echo('Closing file...')
 
     nodeAddr = None
@@ -102,9 +113,9 @@ def retrieve(filename, keysfilename):
             # # # # Call api
             data = requests.post(url = f'http://{nodeAddr}:{nodePort+1}', json = {'JSONRPCMethod':'dht_getValue', 'Key':key, 'Value':''})
             # Write to keysFileName
-            chunk = data.text
-            print('ret text', chunk)
-            f.write(f'{chunk}')
+            chunk = Fernet(fernet_key).decrypt(data.text.encode())
+            # print('ret text', chunk)
+            f.write(chunk)
 
     click.echo('Download complete!')
 
